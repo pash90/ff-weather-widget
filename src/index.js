84a2f81e15f6440d3e42100686b4767db72cc3e4
Parth Mehta <tech@iamdifferent.io>
2018-11-09 11:55:48 +1100

    :tada: base project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/** Libraries */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/** Components */
+import App from './App';
+
+/** Styles */
+import './index.scss';
+
+ReactDOM.render(<App />, document.getElementById('root'));
