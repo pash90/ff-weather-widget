0fdaa9bec1e3fd538faf177029b3aa0779403d3f
Parth Mehta <tech@iamdifferent.io>
2018-11-09 13:57:42 +1100

    :lipstick: minor style changes

--- src/App/Widget.js
+++ src/App/Widget.js
@@ -81,7 +81,7 @@ class Widget extends React.Component {
 				<div className="widget">
 					<h3>{title.length > 0 ? title : 'TITLE OF WIDGET'}</h3>
 
-					<div className="flex center margin-top-2">
+					<div className="flex center margin-top-3">
 						<img className="icon margin-right-2" src={ICON_BASE_URL + weather[0].icon + '.png'} />
 
 						<div className="flex col">
